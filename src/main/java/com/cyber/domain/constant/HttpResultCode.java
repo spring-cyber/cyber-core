ca618786676a8a5ad765d3706fc9132f49f54f3b
yanwei001 <tyfrost@yanweideMacBook-Pro.local>
2023-03-30 18:33:14 +0800

    常量修改

--- src/main/java/com/cyber/domain/constant/HttpResultCode.java
+++ src/main/java/com/cyber/domain/constant/HttpResultCode.java
@@ -1,21 +1,21 @@
 package com.cyber.domain.constant;
 
 public enum HttpResultCode implements ResultCode {
-    SUCCESS(200, "0"),
-    VALIDATE_ERROR(400, "000400"),
-    BAD_AUTH(401, "000401"),
-    NOT_RIGHT(403, "000403"),
-    NOT_FOUND(404, "000404"),
-    REST_METHOD_NOT_SUPPORT(405, "000405"),
-    REQUEST_BODY_NULL(406, "000406"),
-    PARAM_ERROR(412, "000412"),
-    RECORD_NOT_EXIST(413, "000413"),
-    RECORD_EXIST(414, "000414"),
-    SERVER_ERROR(500, "000500"),
-    BAD_SQL_ERROR(501, "000501"),
-    SERVICE_INVOKE_EXCEPTION(502, "000502"),
-    FILE_UPLOAD_ERROR(503, "000503"),
-    FILE_DOWNLOAD_ERROR(504, "000504");
+    SUCCESS(200, "Success"),
+    VALIDATE_ERROR(400, "Bad Request"),
+    BAD_AUTH(401, "Unauthorized"),
+    NOT_RIGHT(403, "Forbidden"),
+    NOT_FOUND(404, "Not Found"),
+    REST_METHOD_NOT_SUPPORT(405, "Method Not Allowed"),
+    REQUEST_BODY_NULL(406, "Request Body Null"),
+    PARAM_ERROR(412, "Param Error"),
+    RECORD_NOT_EXIST(413, "Data Not Exist"),
+    RECORD_EXIST(414, "Data Already Exist"),
+    SERVER_ERROR(500, "Internal Server Error"),
+    BAD_SQL_ERROR(501, "Bad Sql Error"),
+    SERVICE_INVOKE_EXCEPTION(502, "Service Invoke Exception"),
+    FILE_UPLOAD_ERROR(503, "File Upload Error"),
+    FILE_DOWNLOAD_ERROR(504, "File Download Error");
 
 
     private final int code;
